6f813308063e958c48d19e698f9970e9a31bb55d
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-09-16 14:51:22 -0600

    Adds conditional component rendering

---
+++ Practice_2/src/components/Author.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>The Author</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
+
+<!-- The sytling of the child applies, even if data is passed from the parent to the child -->
