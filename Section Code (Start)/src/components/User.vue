3020b0f23c7e17f7c87c429fb8d516c27ce63764
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-09-16 13:08:34 -0600

    Adds custom events, adds an eventBus and new Vue instance to centralize data.

--- Section Code (Start)/src/components/User.vue
+++ Section Code (Start)/src/components/User.vue
@@ -4,16 +4,24 @@
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change My Name</button>
         <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
               <!-- The colon aka the v-bind shortcut :name passes data from the parent to the child and makes the name dynamic rather than hard coded -->
               <!-- nameWasReset event refers to data passed through the event and passing through the updated name -->
-                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
+                <app-user-detail
+                    :myName="name"
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"></app-user-detail>
 
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit
+                  :userAge="age"
+                  @ageWasEdited="age = $event"
+                ></app-user-edit>
             </div>
         </div>
     </div>
@@ -26,12 +34,16 @@
     export default {
       data: function() {
         return {
-          name: 'Mackenzie'
+          name: 'Mackenzie',
+          age: 99
         };
       },
       methods: {
         changeName() {
-          this.name = 'Maximillian';
+          this.name = 'Anna';
+        },
+        resetName() {
+          this.name = 'Max'
         }
       },
       components: {
--- Section Code (Start)/src/components/UserDetail.vue
+++ Section Code (Start)/src/components/UserDetail.vue
@@ -3,16 +3,22 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name: {{ switchName() }}</p>
+        <p>User Age: {{ userAge }}</p>
         <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
     </div>
 </template>
 
 <script>
+  import { eventBus } from '../main';
+
   export default {
     props: {
       myName: {
         type: String
-      }
+      },
+      resetFn: Function,
+      userAge: Number
     },
     methods: {
       switchName() {
@@ -25,6 +31,11 @@
         // We can pass data in the  second argument
         // In the parent component (user vue file we can listen for the event)
       }
+    },
+    created() {
+      eventBus.$on('ageWasEdited', (age) => {
+        this.userAge = age;
+      });
     }
   }
 </script>
